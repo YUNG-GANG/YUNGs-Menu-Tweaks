2131bcf517c3ba75df72f5461c8151b600f69023
drullkus <drullkus@gmail.com>
2024-04-20 12:08:57 -0700

    Create NeoForge subproject

---
+++ NeoForge/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/config/YMTConfigNeoForge.java
@@ -0,0 +1,47 @@
+package com.yungnickyoung.minecraft.yungsmenutweaks.config;
+
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class YMTConfigNeoForge {
+    public static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    public static final ModConfigSpec SPEC;
+
+    public static final ModConfigSpec.ConfigValue<Boolean> enableRightClickCycleButton;
+    public static final ModConfigSpec.ConfigValue<Boolean> enableMouseScrollOnSliders;
+    public static final ModConfigSpec.ConfigValue<Boolean> enableBackgroundTexture;
+    public static final ModConfigSpec.ConfigValue<String> backgroundTexture;
+
+    static {
+        BUILDER.push("YUNG's Menu Tweaks");
+
+        enableRightClickCycleButton = BUILDER
+                .comment(
+                        " Allows right clicking to cycle backwards through some buttons.\n" +
+                        " Default: true")
+                .define("Right Click Reverses Cycle Buttons", true);
+
+        enableMouseScrollOnSliders = BUILDER
+                .comment(
+                        " Allows scrolling the mouse wheel to modify options using a slider\n" +
+                        " when hovering over the slider.\n" +
+                        " Default: true")
+                .define("Mouse Scroll Wheel Affects Sliders", true);
+
+        enableBackgroundTexture = BUILDER
+                .comment(
+                        " If enabled, a custom background texture will be used where possible.\n" +
+                        " The texture used is specified by the Custom Background Texture option below.\n" +
+                        " Default: true")
+                .define("Enable Custom Background Texture", false);
+
+        backgroundTexture = BUILDER
+                .comment(
+                        " A 16x16 texture to use as a custom background, instead of the typical dirt.\n" +
+                        " Only does anything if the Enable Custom Background Texture option is true.\n" +
+                        " Default: minecraft:textures/block/dirt.png")
+                .define("Custom Background Texture", "minecraft:textures/block/dirt.png");
+
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+}
---
+++ NeoForge/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/module/ConfigModuleNeoForge.java
@@ -0,0 +1,43 @@
+package com.yungnickyoung.minecraft.yungsmenutweaks.module;
+
+import com.yungnickyoung.minecraft.yungsmenutweaks.YungsMenuTweaksCommon;
+import com.yungnickyoung.minecraft.yungsmenutweaks.YungsMenuTweaksNeoForge;
+import com.yungnickyoung.minecraft.yungsmenutweaks.config.YMTConfigNeoForge;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.LevelEvent;
+
+public class ConfigModuleNeoForge {
+    public static void init() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, YMTConfigNeoForge.SPEC, "yungsmenutweaks-neoforge-1_20_4.toml");
+        NeoForge.EVENT_BUS.addListener(ConfigModuleNeoForge::onWorldLoad);
+        YungsMenuTweaksNeoForge.loadingContextEventBus.addListener(ConfigModuleNeoForge::onConfigChange);
+    }
+
+    private static void onWorldLoad(LevelEvent.Load event) {
+        bakeConfig();
+    }
+
+    private static void onConfigChange(ModConfigEvent event) {
+        if (event.getConfig().getSpec() == YMTConfigNeoForge.SPEC) {
+            bakeConfig();
+        }
+    }
+
+    private static void bakeConfig() {
+        YungsMenuTweaksCommon.CONFIG.enableRightClickCycleButton = YMTConfigNeoForge.enableRightClickCycleButton.get();
+        YungsMenuTweaksCommon.CONFIG.enableMouseScrollOnSliders = YMTConfigNeoForge.enableMouseScrollOnSliders.get();
+        YungsMenuTweaksCommon.CONFIG.enableBackgroundTexture = YMTConfigNeoForge.enableBackgroundTexture.get();
+        ResourceLocation backgroundTexture = ResourceLocation.tryParse(YMTConfigNeoForge.backgroundTexture.get());
+        if (backgroundTexture != null) {
+            YungsMenuTweaksCommon.CONFIG.backgroundTexture = backgroundTexture;
+        } else {
+            YungsMenuTweaksCommon.LOGGER.error("Invalid background texture path: " + YMTConfigNeoForge.backgroundTexture.get());
+            YungsMenuTweaksCommon.LOGGER.error("Using default background texture path instead: textures/block/dirt.png");
+            YungsMenuTweaksCommon.CONFIG.backgroundTexture = new ResourceLocation("textures/block/dirt.png");
+        }
+    }
+}
