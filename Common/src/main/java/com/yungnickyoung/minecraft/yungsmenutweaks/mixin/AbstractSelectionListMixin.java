84489b934ec5c0c50150afef083772042e7d5aa4
Nick Young <yungnickyoung@gmail.com>
2024-07-24 17:42:49 -0400

    Remove commented out code from porting.

--- Common/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/mixin/AbstractSelectionListMixin.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/mixin/AbstractSelectionListMixin.java
@@ -2,26 +2,13 @@ package com.yungnickyoung.minecraft.yungsmenutweaks.mixin;
 
 import com.yungnickyoung.minecraft.yungsmenutweaks.YungsMenuTweaksCommon;
 import net.minecraft.client.gui.components.AbstractSelectionList;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractSelectionList.class)
 public class AbstractSelectionListMixin {
-//    @ModifyArg(
-//            method = "renderListBackground",
-//            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V"), index = 0)
-//    public ResourceLocation yungsmenutweaks_changeSelectionListBackground(ResourceLocation original) {
-//        if (YungsMenuTweaksCommon.CONFIG.enableBackgroundTexture) {
-//            return YungsMenuTweaksCommon.CONFIG.backgroundTexture;
-//        }
-//        return original;
-//    }
-
     /**
      * Registers right-clicking all selection entry buttons as a valid click action.
      */
