9157dc26bef055d4cc748ccc7e96b58b3f8dd470
Nick Young <yungnickyoung@gmail.com>
2025-02-04 15:15:13 -0500

    v2.1.2. No longer crashes when loaded on dedicated server.

--- Forge/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/YungsMenuTweaksForge.java
+++ Forge/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/YungsMenuTweaksForge.java
@@ -1,10 +1,12 @@
 package com.yungnickyoung.minecraft.yungsmenutweaks;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(YungsMenuTweaksCommon.MOD_ID)
 public class YungsMenuTweaksForge {
     public YungsMenuTweaksForge() {
-        YungsMenuTweaksCommon.init();
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> YungsMenuTweaksCommon::init);
     }
 }
--- NeoForge/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/YungsMenuTweaksNeoForge.java
+++ NeoForge/src/main/java/com/yungnickyoung/minecraft/yungsmenutweaks/YungsMenuTweaksNeoForge.java
@@ -1,11 +1,12 @@
 package com.yungnickyoung.minecraft.yungsmenutweaks;
 
 import com.yungnickyoung.minecraft.yungsmenutweaks.module.ConfigModuleNeoForge;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
-@Mod(YungsMenuTweaksCommon.MOD_ID)
+@Mod(value = YungsMenuTweaksCommon.MOD_ID, dist = Dist.CLIENT)
 public class YungsMenuTweaksNeoForge {
     public static IEventBus loadingContextEventBus;
 
